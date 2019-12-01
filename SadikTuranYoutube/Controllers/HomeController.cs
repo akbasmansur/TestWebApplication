7eb6fcd7e3a90e347483e5be2ba14648747c8468
Mansur Akbas <mansurakbas05@gmail.com>
2019-12-01 22:23:33 +0300

    Commit 01 Dec 2019

--- SadikTuranYoutube/Controllers/HomeController.cs
+++ SadikTuranYoutube/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using SadikTuranYoutube.Models;
 
 namespace SadikTuranYoutube.Controllers
 {
@@ -10,7 +7,8 @@ namespace SadikTuranYoutube.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var course = new Course { Id = 323, Name = "Asp Net Kursu" };
+            return View(course);
         }
     }
 }
